fcb4f267080a9a3f11fd8899f73389aa03d29054
tuanlong1202 <tuanlong1202@gmail.com>
2021-03-20 20:01:32 -0400

    Phase 0 intro to js 2 looping code along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+   
+for(let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy brithday to me!`);
+}
+
+function writeCards(nameList,eventName) {
+    var arrMessages = [];
+    for (let i = 0; i < nameList.length; i++) {
+        arrMessages[i] = `Thank you, ${nameList[i]}, for the wonderful ${eventName} gift!` ;
+    }
+    return arrMessages;
+}
+
+function countDown (provideNumber) {
+    let i = provideNumber;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
